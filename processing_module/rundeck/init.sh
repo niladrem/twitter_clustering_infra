194f8a88e188e6f21aadd7b6f255c45e60fae66b
Anna Urbala <aurbala@gmail.com>
2021-10-25 17:56:50 +0200

    rundeck job

--- processing_module/rundeck/init.sh
+++ processing_module/rundeck/init.sh
@@ -4,4 +4,9 @@
 # https://stackoverflow.com/questions/65037081/auto-create-rundeck-jobs-on-startup-rundeck-in-docker-container
 sleep 120
 
-curl -X POST rundeck:4440/api/14/projects -H "X-Rundeck-Auth-Token:testtoken" -H "Content-Type: application/json" --data '{"name": "twitter"}'
+export RD_TOKEN="testtoken"
+export RD_URL="rundeck:4440"
+export RD_JOB_PATH="/etc/rundeck"
+
+curl -X POST "$RD_URL/api/14/projects" -H "X-Rundeck-Auth-Token:$RD_TOKEN" -H "Content-Type: application/json" --data '{"name": "twitter"}'
+curl -kSsv -H "X-Rundeck-Auth-Token:$RD_TOKEN" -H "Content-Token: application/yaml" -H "fileformat: yaml" -F xmlBatch=@"$RD_JOB_PATH/Download_tweets.yaml" "$RD_URL/api/14/project/twitter/jobs/import?fileformat=yaml&dupeOption=update"
