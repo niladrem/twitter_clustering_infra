f15664f77a8e863f4aca5fd37351904786c17223
Anna Urbala <aurbala@gmail.com>
2021-11-07 22:28:18 +0100

    mention

--- processing_module/scripts/download_tweets.py
+++ processing_module/scripts/download_tweets.py
@@ -1,3 +1,4 @@
+import time
 import tweepy
 import argparse
 import pickle
@@ -5,7 +6,7 @@ import uuid
 from datetime import datetime, timedelta, timezone
 
 users_dict = {}
-relations_dict = {"follow": [], "friend": [], "retweet": [], "mention": [], "response": [], "like": []}
+relations_dict = {"follow": [], "friend": [], "retweet": [], "mention": [], "like": [], "reply": [], "quote": []}
 users_to_process = []
 processed_users = []
 
@@ -15,54 +16,55 @@ def add_user(user):
     users_dict[user.id] = user
 
 
-def process_simple_relation(relation_type, src_user_id, dst_user):
-    add_user(dst_user)
-    relations_dict[relation_type].append({"src_user_id": src_user_id, "dst_user_id": dst_user.id})
+def process_simple_relation(relation_type, src_user_id, dst_user_id, text):
+    relations_dict[relation_type].append({"src_user_id": src_user_id, "dst_user_id": dst_user_id, "text": text})
 
 
 def process_post(api, post):
     # quote - response/retweet?
     src_user_id = post.user.id
-    post_type = "none"
-    dst_user_id = None
-    text = post.text
     if post.is_quote_status:
-        add_user(post.quoted_status.user)
-        post_type = "response"
-        dst_user_id = post.quoted_status.user.id
-    elif post.retweeted_status is not None:
+        status = api.lookup_statuses([post.quoted_status_id])[0]
+        add_user(status.user)
+        process_simple_relation("quote", src_user_id, status.user.id, post.text)
+    if post.retweeted:
         add_user(post.retweeted_status.user)
-        post_type = "retweet"
-        dst_user_id = post.retweeted_status.user.id
-        src_user_id, dst_user_id = dst_user_id, src_user_id
-    # @TODO add mention
+        process_simple_relation("retweet", post.retweeted_status.user.id, src_user_id, post.text)
+    if post.in_reply_to_status_id is not None:
+        status = api.lookup_statuses([post.in_reply_to_status_id])[0]
+        add_user(status.user)
+        process_simple_relation("reply", src_user_id, status.user.id, post.text)
+    for entity in post.entities['user_mentions']:
+        mentioned_user = api.get_user(entity.id)
+        add_user(mentioned_user)
+        process_simple_relation("mention", src_user_id, user.id, post.text)
 
-    if post_type == "none":
-        # get retweeters
-        pass
+
+    # get retweeters @TODO
     print(post)
 
 
-def process_user(api, user_id):
+def process_user(api, user_id, extra_info):
     # user timeline
     posts = api.user_timeline(user_id=user_id, count=200)
     for post in posts:
         process_post(api, post)
 
-    # followers
-    followers = api.get_followers(user_id=user_id, count=200)
-    for follower in followers:
-        process_simple_relation("follow", user_id, follower)
-
-    # friends
-    friends = api.get_friends(user_id=user_id, count=200)
-    for friend in friends:
-        process_simple_relation("friend", user_id, follower)
-
     # favorites
     favorites = api.get_favorites(user_id=user_id, count=200)
     for favorite in favorites:
-        process_simple_relation("like", user_id, follower)
+        process_simple_relation("like", user_id, favorite, None)
+
+    if extra_info:
+        # followers
+        followers = api.get_followers(user_id=user_id, count=200)
+        for follower in followers:
+            process_simple_relation("follow", user_id, follower, None)
+
+        # friends
+        friends = api.get_friends(user_id=user_id, count=200)
+        for friend in friends:
+            process_simple_relation("friend", user_id, friend, None)
 
 
 if __name__ == "__main__":
@@ -75,6 +77,7 @@ if __name__ == "__main__":
     parser.add_argument('--query', default='covid')
     parser.add_argument('--hours', type=int, default=3)
     parser.add_argument('--path', default="/shared/data/json/")
+    parser.add_argument('--extra-info', type=bool, default=False)
 
     args = parser.parse_args()
 
@@ -101,7 +104,7 @@ if __name__ == "__main__":
     while len(users_to_process) > 0 and len(processed_users) < 1:
         user = users_to_process.pop(0)
         if user not in processed_users:
-            process_user(api, user)
+            process_user(api, user, args.extra_info)
             processed_users.append(user)
 
 print("---USER DICT---")
