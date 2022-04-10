31db32cfff2f31e8a42eb04d018d6a743d034fe3
Anna Urbala <aurbala@gmail.com>
2022-04-10 22:40:20 +0200

    rundeck - health

--- processing_module/rundeck/init.sh
+++ processing_module/rundeck/init.sh
@@ -2,11 +2,20 @@
 
 # https://docs.rundeck.com/docs/api/rundeck-api.html
 # https://stackoverflow.com/questions/65037081/auto-create-rundeck-jobs-on-startup-rundeck-in-docker-container
-sleep 240
 
 export RD_TOKEN="testtoken"
 export RD_URL="rundeck:4440"
 export RD_JOB_PATH="/etc/rundeck"
+
+while true
+do
+	sleep 30
+	echo "Check rundeck health"
+	HEALTH=$(curl "$RD_URL/health")
+	if [ "$HEALTH" = "ok" ]; then
+    break
+  fi
+done
 
 curl -X POST "$RD_URL/api/14/projects" -H "X-Rundeck-Auth-Token:$RD_TOKEN" -H "Content-Type: application/json" --data '{"name": "twitter"}'
 curl -kSsv -H "X-Rundeck-Auth-Token:$RD_TOKEN" -H "Content-Token: application/yaml" -H "fileformat: yaml" -F xmlBatch=@"$RD_JOB_PATH/Download_tweets.yaml" "$RD_URL/api/14/project/twitter/jobs/import?fileformat=yaml&dupeOption=update"
